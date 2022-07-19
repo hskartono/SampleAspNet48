0acf78dfe58cfd1b74f8acb096444daffa67b06c
hskartono <hskartono@gmail.com>
2022-07-19 15:02:23 +0700

    Add project files.

---
+++ TrainingAspNet/Controllers/MovieController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrainingAspNet.Models;
+
+namespace TrainingAspNet.Controllers
+{
+    public class MovieController : Controller
+    {
+        private static List<Movie> movieList = new List<Movie>();
+        private static int id = 0;
+
+        public MovieController()
+        {
+            InitData();
+        }
+
+        private void InitData()
+        {
+            if (movieList.Count == 0)
+            {
+                var movie1 = new Movie();
+                movie1.Id = 1;
+                movie1.Title = "Rambo 4";
+                movie1.Actrees = "Silverster Stalone";
+                movie1.Rating = 8;
+                movie1.MovieYear = 2000;
+                movieList.Add(movie1);
+
+                var movie2 = new Movie()
+                {
+                    Id = 2,
+                    Title = "Shark",
+                    Actrees = "James",
+                    Rating = 9
+                };
+                movieList.Add(movie2);
+
+                movieList.Add(new Movie()
+                {
+                    Id = 3,
+                    Title = "Batman",
+                    Actrees = "John",
+                    Rating = 8
+                });
+
+                movieList.Add(
+                    new Movie(4, "Robin", "Doe", 8.5)
+                );
+
+                id = 4;
+            }
+        }
+
+        public ActionResult Index()
+        {
+            return View(movieList);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            Movie movie = new Movie();
+            return View(movie);
+        }
+
+        [HttpPost]
+        public ActionResult Create(FormCollection param)
+        {
+            id++;
+            Movie movie = new Movie();
+            movie.Id = id;
+            movie.Title = param["Title"];
+            movie.Actrees = param["Actrees"];
+
+            double rating = 0;
+            if (double.TryParse(param["Rating"], out rating) == true)
+            {
+                movie.Rating = rating;
+            } else
+            {
+                movie.Rating = 0;
+            }
+
+            movieList.Add(movie);
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int id)
+        {
+            if(movieList.Where(e=>e.Id == id).Any() == false)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var data = movieList.Where(e => e.Id == id).First();
+            return View(data);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            if (movieList.Where(e => e.Id == id).Any() == false)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var data = movieList.Where(e => e.Id == id).First();
+            movieList.Remove(data);
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            if (movieList.Where(e => e.Id == id).Any() == false)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var data = movieList.Where(e => e.Id == id).First();
+            return View(data);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(FormCollection param, int id)
+        {
+            if (movieList.Where(e => e.Id == id).Any() == false)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var data = movieList.Where(e => e.Id == id).First();
+
+            data.Title = param["Title"];
+            data.Actrees = param["Actrees"];
+
+            double rating = 0;
+            if (double.TryParse(param["Rating"], out rating) == true)
+            {
+                data.Rating = rating;
+            }
+            else
+            {
+                data.Rating = 0;
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ TrainingAspNet/Models/Movie.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrainingAspNet.Models
+{
+    public class Movie
+    {
+        public Movie()
+        {
+
+        }
+
+        public Movie(int id, string title, string actrees, double rating)
+        {
+            Id = id;
+            Title = title;
+            Actrees = actrees;
+            Rating = rating;
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Actrees { get; set; }
+        public double Rating { get; set; }
+
+        private int mMovieYear;
+
+        public int MovieYear
+        {
+            get { 
+                // something doing here
+                return mMovieYear; 
+            }
+            set { 
+                mMovieYear = value; 
+            }
+        }
+
+
+    }
+}
